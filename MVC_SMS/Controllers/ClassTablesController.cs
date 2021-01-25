6640181954b499436429a972f81a21fd9f735dd5
trombone8417 <trombone8417@yahoo.com>
2021-01-26 07:39:25 +0800

    ClassTables

---
+++ MVC_SMS/Controllers/ClassTablesController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseAccess;
+
+namespace MVC_SMS.Controllers
+{
+    public class ClassTablesController : Controller
+    {
+        private SchoolMgtDbEntities db = new SchoolMgtDbEntities();
+
+        // GET: ClassTables
+        public ActionResult Index()
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            return View(db.ClassTables.ToList());
+        }
+
+        // GET: ClassTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClassTable classTable = db.ClassTables.Find(id);
+            if (classTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classTable);
+        }
+
+        // GET: ClassTables/Create
+        public ActionResult Create()
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            return View();
+        }
+
+        // POST: ClassTables/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ClassTable classTable)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                db.ClassTables.Add(classTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(classTable);
+        }
+
+        // GET: ClassTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClassTable classTable = db.ClassTables.Find(id);
+            if (classTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classTable);
+        }
+
+        // POST: ClassTables/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ClassTable classTable)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(classTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(classTable);
+        }
+
+        // GET: ClassTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClassTable classTable = db.ClassTables.Find(id);
+            if (classTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classTable);
+        }
+
+        // POST: ClassTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            ClassTable classTable = db.ClassTables.Find(id);
+            db.ClassTables.Remove(classTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
