bf843aa97ab8335077c7d164f3cabca01719c5d4
trombone8417 <trombone8417@yahoo.com>
2021-01-28 07:34:55 +0800

    ClassSubjectTable

---
+++ MVC_SMS/Controllers/ClassSubjectTablesController.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseAccess;
+
+namespace MVC_SMS.Controllers
+{
+    public class ClassSubjectTablesController : Controller
+    {
+        private SchoolMgtDbEntities db = new SchoolMgtDbEntities();
+
+        // GET: ClassSubjectTables
+        public ActionResult Index()
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            var classSubjectTables = db.ClassSubjectTables.Include(c => c.ClassTable).Include(c => c.SubjectTable);
+            return View(classSubjectTables.ToList());
+        }
+
+        // GET: ClassSubjectTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClassSubjectTable classSubjectTable = db.ClassSubjectTables.Find(id);
+            if (classSubjectTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classSubjectTable);
+        }
+
+        // GET: ClassSubjectTables/Create
+        public ActionResult Create()
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            ViewBag.ClassID = new SelectList(db.ClassTables.Where(c=>c.IsActive==true), "ClassID", "Name");
+            ViewBag.SubjectID = new SelectList(db.SubjectTables, "SubjectID", "Name");
+            return View();
+        }
+
+        // POST: ClassSubjectTables/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ClassSubjectTable classSubjectTable)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (ModelState.IsValid)
+            {
+                var classname = db.ClassTables.Where(c => c.ClassID == classSubjectTable.ClassID).SingleOrDefault();
+                if (classname != null)
+                {
+                    if (!classSubjectTable.Name.Contains(classname.Name))
+                    {
+                        classSubjectTable.Name = classSubjectTable.Name + "-" + classname.Name;
+                    }
+                }
+                db.ClassSubjectTables.Add(classSubjectTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClassID = new SelectList(db.ClassTables.Where(c => c.IsActive == true), "ClassID", "Name", classSubjectTable.ClassID);
+            ViewBag.SubjectID = new SelectList(db.SubjectTables, "SubjectID", "Name", classSubjectTable.SubjectID);
+            return View(classSubjectTable);
+        }
+
+        // GET: ClassSubjectTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClassSubjectTable classSubjectTable = db.ClassSubjectTables.Find(id);
+            if (classSubjectTable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClassID = new SelectList(db.ClassTables.Where(c => c.IsActive == true), "ClassID", "Name", classSubjectTable.ClassID);
+            ViewBag.SubjectID = new SelectList(db.SubjectTables, "SubjectID", "Name", classSubjectTable.SubjectID);
+            return View(classSubjectTable);
+        }
+
+        // POST: ClassSubjectTables/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ClassSubjectTable classSubjectTable)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (ModelState.IsValid)
+            {
+                var classname = db.ClassTables.Where(c => c.ClassID == classSubjectTable.ClassID).SingleOrDefault();
+                if (classname != null)
+                {
+                    if (!classSubjectTable.Name.Contains(classname.Name))
+                    {
+                        classSubjectTable.Name = classSubjectTable.Name + "-" + classname.Name;
+                    }
+                }
+                db.Entry(classSubjectTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClassID = new SelectList(db.ClassTables.Where(c => c.IsActive == true), "ClassID", "Name", classSubjectTable.ClassID);
+            ViewBag.SubjectID = new SelectList(db.SubjectTables, "SubjectID", "Name", classSubjectTable.SubjectID);
+            return View(classSubjectTable);
+        }
+
+        // GET: ClassSubjectTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClassSubjectTable classSubjectTable = db.ClassSubjectTables.Find(id);
+            if (classSubjectTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classSubjectTable);
+        }
+
+        // POST: ClassSubjectTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            ClassSubjectTable classSubjectTable = db.ClassSubjectTables.Find(id);
+            db.ClassSubjectTables.Remove(classSubjectTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
