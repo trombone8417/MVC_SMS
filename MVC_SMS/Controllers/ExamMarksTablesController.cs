42ece3fbc241bd7737e4e5b7fd08db77ccdf643e
trombone8417 <trombone8417@yahoo.com>
2021-02-01 08:13:21 +0800

    ExamMarksTable &  ExamTable

---
+++ MVC_SMS/Controllers/ExamMarksTablesController.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseAccess;
+
+namespace MVC_SMS.Controllers
+{
+    public class ExamMarksTablesController : Controller
+    {
+        private SchoolMgtDbEntities db = new SchoolMgtDbEntities();
+
+        // GET: ExamMarksTables
+        public ActionResult Index()
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            var examMarksTables = db.ExamMarksTables.Include(e => e.StudentTable).Include(e => e.UserTable);
+            return View(examMarksTables.ToList());
+        }
+
+        // GET: ExamMarksTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExamMarksTable examMarksTable = db.ExamMarksTables.Find(id);
+            if (examMarksTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(examMarksTable);
+        }
+
+        // GET: ExamMarksTables/Create
+        public ActionResult Create()
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            ViewBag.ExamID = new SelectList(db.ExamTables, "ExamID", "Title");
+            ViewBag.ClassSubjectID = new SelectList(db.ClassSubjectTables, "ClassSubjectID", "Name");
+            ViewBag.StudentID = new SelectList(db.StudentTables, "StudentID", "Name");
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName");
+            return View();
+        }
+
+        // POST: ExamMarksTables/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ExamMarksTable examMarksTable)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+
+            int userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            examMarksTable.UserID = userid;
+            if (ModelState.IsValid)
+            {
+                db.ExamMarksTables.Add(examMarksTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ExamID = new SelectList(db.ExamTables, "ExamID", "Title", examMarksTable.ExamID);
+            ViewBag.ClassSubjectID = new SelectList(db.ClassSubjectTables, "ClassSubjectID", "Name", examMarksTable.ClassSubjectID);
+            ViewBag.StudentID = new SelectList(db.StudentTables, "StudentID", "Name", examMarksTable.StudentID);
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName", examMarksTable.UserID);
+            return View(examMarksTable);
+        }
+
+        // GET: ExamMarksTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExamMarksTable examMarksTable = db.ExamMarksTables.Find(id);
+            if (examMarksTable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ExamID = new SelectList(db.ExamTables, "ExamID", "Title", examMarksTable.ExamID);
+            ViewBag.ClassSubjectID = new SelectList(db.ClassSubjectTables, "ClassSubjectID", "Name", examMarksTable.ClassSubjectID);
+            ViewBag.StudentID = new SelectList(db.StudentTables, "StudentID", "Name", examMarksTable.StudentID);
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName", examMarksTable.UserID);
+            return View(examMarksTable);
+        }
+
+        // POST: ExamMarksTables/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ExamMarksTable examMarksTable)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+
+            int userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            examMarksTable.UserID = userid;
+            if (ModelState.IsValid)
+            {
+                db.Entry(examMarksTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ExamID = new SelectList(db.ExamTables, "ExamID", "Title",examMarksTable.ExamID);
+            ViewBag.ClassSubjectID = new SelectList(db.ClassSubjectTables, "ClassSubjectID", "Name",examMarksTable.ClassSubjectID);
+            ViewBag.StudentID = new SelectList(db.StudentTables, "StudentID", "Name", examMarksTable.StudentID);
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName", examMarksTable.UserID);
+            return View(examMarksTable);
+        }
+
+        // GET: ExamMarksTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExamMarksTable examMarksTable = db.ExamMarksTables.Find(id);
+            if (examMarksTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(examMarksTable);
+        }
+
+        // POST: ExamMarksTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            ExamMarksTable examMarksTable = db.ExamMarksTables.Find(id);
+            db.ExamMarksTables.Remove(examMarksTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC_SMS/Controllers/ExamTablesController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseAccess;
+
+namespace MVC_SMS.Controllers
+{
+    public class ExamTablesController : Controller
+    {
+        private SchoolMgtDbEntities db = new SchoolMgtDbEntities();
+
+        // GET: ExamTables
+        public ActionResult Index()
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            var examTables = db.ExamTables.Include(e => e.UserTable);
+            return View(examTables.ToList());
+        }
+
+        // GET: ExamTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExamTable examTable = db.ExamTables.Find(id);
+            if (examTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(examTable);
+        }
+
+        // GET: ExamTables/Create
+        public ActionResult Create()
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName");
+            return View();
+        }
+
+        // POST: ExamTables/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ExamTable examTable)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+
+            int userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            examTable.UserID = userid;
+            if (ModelState.IsValid)
+            {
+                db.ExamTables.Add(examTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName", examTable.UserID);
+            return View(examTable);
+        }
+
+        // GET: ExamTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExamTable examTable = db.ExamTables.Find(id);
+            if (examTable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName", examTable.UserID);
+            return View(examTable);
+        }
+
+        // POST: ExamTables/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ExamTable examTable)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+
+            int userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            examTable.UserID = userid;
+            if (ModelState.IsValid)
+            {
+                db.Entry(examTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName", examTable.UserID);
+            return View(examTable);
+        }
+
+        // GET: ExamTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExamTable examTable = db.ExamTables.Find(id);
+            if (examTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(examTable);
+        }
+
+        // POST: ExamTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //若未登入
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                //導至登入頁
+                return RedirectToAction("Login", "Home");
+            }
+            ExamTable examTable = db.ExamTables.Find(id);
+            db.ExamTables.Remove(examTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
