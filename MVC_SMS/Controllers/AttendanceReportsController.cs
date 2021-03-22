893f3aa856794dfa382c8e549b68be5217fae813
trombone8417 <trombone8417@yahoo.com>
2021-03-23 07:26:45 +0800

    AttendanceReports

---
+++ MVC_SMS/Controllers/AttendanceReportsController.cs
@@ -0,0 +1,21 @@
+﻿using DatabaseAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_SMS.Controllers
+{
+    public class AttendanceReportsController : Controller
+    {
+        private SchoolMgtDbEntities db = new SchoolMgtDbEntities();
+        // GET: AttendanceReports
+        public ActionResult StudentAttendance(int? id)
+        {
+            var classid = db.StudentPromoteTables.Where(p => p.StudentID == id && p.isActive == true).FirstOrDefault().ClassID;
+            var studentattandance = db.AttendanceTables.Where(a => a.StudentID == id && a.ClassID == classid).OrderByDescending(a => a.AttendanceID);
+            return View(studentattandance);
+        }
+    }
+}
