6564b6d19d4d7b0ae124a4d8e8dd8caecc93377a
trombone8417 <trombone8417@yahoo.com>
2021-03-24 08:01:56 +0800

    StaffAttendance

--- MVC_SMS/Controllers/AttendanceReportsController.cs
+++ MVC_SMS/Controllers/AttendanceReportsController.cs
@@ -11,11 +11,26 @@ namespace MVC_SMS.Controllers
     {
         private SchoolMgtDbEntities db = new SchoolMgtDbEntities();
         // GET: AttendanceReports
+        /// <summary>
+        /// 學生上課點名(個人)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult StudentAttendance(int? id)
         {
             var classid = db.StudentPromoteTables.Where(p => p.StudentID == id && p.isActive == true).FirstOrDefault().ClassID;
             var studentattandance = db.AttendanceTables.Where(a => a.StudentID == id && a.ClassID == classid).OrderByDescending(a => a.AttendanceID);
             return View(studentattandance);
+        }
+        /// <summary>
+        /// 職員打卡紀錄(個人)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult StaffAttendance(int? id)
+        {
+            var staffattandance = db.StaffAttendanceTables.Where(a => a.StaffID == id).OrderByDescending(a => a.StaffID);
+            return View(staffattandance);
         }
     }
 }
